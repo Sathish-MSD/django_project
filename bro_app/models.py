521302334d061684a35345c69d1ded4163a80f5f
Sathish-MSD <131609043+Sathish-MSD@users.noreply.github.com>
2023-08-03 10:25:14 +0530

    Add files via upload

---
+++ bro_app/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+# Create your models here.
+class Order(models.Model):
+    name = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    price = models.IntegerField()
+    availability=models.BooleanField()
+
+
+
+class Product(models.Model):
+    name= models.CharField(max_length=50)
+    price = models.IntegerField(default=50)
+    description = models.TextField(default='Its too yummy')
+    image = models.ImageField(upload_to='images')
+    order_status = models.BooleanField(default=False)
+    items = models.IntegerField(default=0)
+
+
+
+
+def __str__(self):
+    return self.title
